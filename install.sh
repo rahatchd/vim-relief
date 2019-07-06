70b152d41faa4da104b52c2b11a93638e0c8d790
Rahat Dhande <rahatchd@gmail.com>
2019-07-06 11:28:57 -0700

    adds bash_profile support

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ line() {
     printf %$(tput cols)s | tr " " $1 && echo
 }
 
+echo
 line = 
-line \~
 echo "~~~ gruvcube installer ~~~"
 line \~
 echo
@@ -21,18 +21,24 @@ mkdir backup/$backup
 dotfiles=( "bashrc" "bash_aliases" "vimrc" "tmux.conf" )
 for dotfile in "${dotfiles[@]}" ; do
     gruvcube_dotfile="$gruvdir/src/.$dotfile"
-    home_dotfile="$HOME/.$dotfile"
+    home_dotfile=~/."$dotfile"
     backup_dotfile="$gruvdir/backup/$backup/.$dotfile.bak"
-
     if [ -f $home_dotfile ]; then
-        echo "\ backing up $backup_dotfile /"
+        echo "* backing up $backup_dotfile *"
         cp $home_dotfile $backup_dotfile
     fi
-
     cp $gruvcube_dotfile $home_dotfile
 done
 
-source ~/.bashrc
+profile_src="[ -r ~/.bashrc ] && source ~/.bashrc"
+if [ -f ~/.bash_profile ] && ! grep -q "$profile_src" ~/.bash_profile; then
+    echo "< you can now use bashrc in place of bash_profile >"
+    echo "$profile_src" >> ~/.bash_profile
+    source ~/.bash_profile
+else
+    source ~/.bashrc
+fi
+
 read -p "¿ do you want to edit .bashrc ? [Y/n] " yn
 case $yn in
     [Yy]*)
@@ -41,6 +47,7 @@ case $yn in
     *)
         echo "you can can edit your .bashrc anytime using the command bashrc"
 esac
+
 read -p "¿ do you want to edit .bash_aliases ? [Y/n] " yn
 case $yn in
     [Yy]*)
@@ -49,8 +56,9 @@ case $yn in
     *)
         echo "you can can edit your .aliases anytime using the command customrc"
 esac
+
 if [ ! -f ~/.customrc ]; then
-    cp $gruvdir/src/.customrc $HOME/.customrc
+    cp $gruvdir/src/.customrc ~/.customrc
     read -p "¿ do you want to edit .customrc (not tracked by gruvcube) ? [Y/n] " yn
     case $yn in
         [Yy]*)
@@ -60,8 +68,9 @@ if [ ! -f ~/.customrc ]; then
             echo "you can can edit your .customrc anytime using the command customrc"
     esac
 fi
+
 if [ ! -f ~/.secretsrc ]; then
-    cp $gruvdir/src/.secretsrc $HOME/.secretsrc
+    cp $gruvdir/src/.secretsrc ~/.secretsrc
     read -p "¿ do you want to edit .secretsrc (not tracked by gruvcube) ? [Y/n] " yn
     case $yn in
         [Yy]*)
@@ -71,22 +80,23 @@ if [ ! -f ~/.secretsrc ]; then
             echo "you can can edit your .secretsrc anytime using the command secretsrc"
     esac
 fi
-if [ ! -d "$HOME/.vim/bundle/Vundle.vim"  ]; then
+
+if [ ! -d ~/.vim/bundle/Vundle.vim  ]; then
     echo "> installing Vundle (may require git login) <"
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 vim +PluginInstall +qall
 vim_python=$(vim --version | grep "+python")
 if [[ $(vim --version | grep "+python3") ]]; then
-    echo "> if you haven't already, manually install ycm (code autocompletion) using python 3.5 <"
+    echo "< if you haven't already, manually install ycm (code autocompletion) using python 3.5 >"
 elif [[ $(vim --version | grep "+python") ]]; then
-    echo "> if you havent' already, mannualy install ycm (code autocompletion) using python 2.7 <"
+    echo "< if you havent' already, mannualy install ycm (code autocompletion) using python 2.7 >"
 else
     echo "¡ cannot install ycm (code autocompletion) if vim was built without python support !"
 fi
 
-if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-    echo "> installing tmux-plugin-manager (may require git login) <"
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    echo "< installing tmux-plugin-manager (may require git login) >"
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 if [ -n "$TMUX" ]; then
@@ -98,6 +108,6 @@ fi
 echo
 line -
 echo "~~~ DONE ~~~"
-line -
 line = 
+echo
 
